b40d7fa864be3fed1fb74e74707b775e3169d273
aarushshah2008 <005141@usgschool.org>
2020-09-22 19:50:35 +0530

    fixbug

--- sketch.js
+++ sketch.js
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+   // if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+   // }
 }
 
 
@@ -95,11 +95,13 @@ function mouseReleased(){
 function keyPressed(){
     if(keyCode === 32){
        slingshot.attach(bird.body);
+       bird.trajectory=[]
+       Matter.Body.setPosition(bird.body,{x:200,y:50})
     }
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");                                                                             
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
